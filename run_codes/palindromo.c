21b07606c66d421c44126e28a10e5a3984c0e516
Caio Petroncini <caiopetroncini@gmail.com>
2024-05-07 23:00:17 -0300

    another day, another banger

--- run_codes/palindromo.c
+++ run_codes/palindromo.c
@@ -2,41 +2,21 @@
 
 
 int main(void){
-    int n;
+    int q;
     char in;
     int pal = 0;
 
-    scanf("%d", &n);
+    scanf("%d ", &q);
 
-    for (int i = 0; i < n; i++)
-    {
-        scanf(" %c", &in);
-        //pal = pal^power(2, in - 'a');
-        if(in - 'a' == 0){
-            pal = pal^1;
-        } else {
-            pal = pal^(2 << (in-'a' - 1));
-        }
-    }
-
-    if(pal == 0){
+    do{
+        scanf("%c", &in);
+        if(in >= 'a') pal = pal^(1 << (in - 'a'));
+        
+    } while(in != '\n');
+    
+    if((pal & (pal - 1)) == 0){
         printf("Sim\n");
     } else {
-        for (int i = 0; i < n; i++)
-        {
-            int power;
-            if(i == 0){
-                power = 1;
-            } else {
-                power = 2 << (i - 1);
-            }
-            if(pal == power){
-                printf("Sim\n");
-                return 0;
-            }
-        }
         printf("Nao\n");
-        
     }
-
 }
